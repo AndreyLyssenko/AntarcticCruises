0d8f707746f654bbff2f7f2f83525edb8433dafb
Andrey Lyssenko <andreylyssenko95@gmail.com>
2022-08-05 18:21:24 +0600

    Исправления по баг-листу

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -2,10 +2,23 @@ const form = document.querySelector('.booking__form-wrapper').querySelector('for
 const inputName = document.querySelector('#name');
 const inputPhone = document.querySelector('#phone');
 const inputEmail = document.querySelector('#email');
+const formErrorMessage = form.querySelector('.booking__message');
+const re = /^[\+]?[0-9]*$/;
 
-form.addEventListener('submit', () => {
-  localStorage.clear();
-  localStorage.setItem('name', inputName.value);
-  localStorage.setItem('phone', inputPhone.value);
-  localStorage.setItem('email', inputEmail.value);
+inputPhone.addEventListener('focus', () => {
+  formErrorMessage.classList.remove('booking__message--error');
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  if (!re.test(inputPhone.value)) {
+    formErrorMessage.classList.toggle('booking__message--error');
+  } else {
+    localStorage.clear();
+    localStorage.setItem('name', inputName.value);
+    localStorage.setItem('phone', inputPhone.value);
+    localStorage.setItem('email', inputEmail.value);
+    evt.target.submit();
+  }
 });
--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -1,9 +1,12 @@
 const navMain = document.querySelector('.navigation__list');
 const navToggle = document.querySelector('.navigation__button');
-const navWrapper = document.querySelector('.navigation');
+const nav = document.querySelector('.navigation');
+const navWrapper = document.querySelector('.navigation__wrapper--no-js');
 const navLogo = document.querySelector('.navigation__logo-link');
 const greetings = document.querySelector('.greetings');
 const navLinks = document.querySelectorAll('.navigation__link');
+const pageBody = document.querySelector('.page__body');
+const navHeadline = document.querySelector('.navigation__headline');
 
 navMain.classList.remove('navigation__list--no-js');
 navMain.classList.add('navigation__list--closed');
@@ -11,7 +14,8 @@ navToggle.classList.remove('navigation__button--no-js');
 navToggle.classList.add('navigation__button--closed');
 greetings.classList.remove('greetings--no-js');
 navLogo.classList.remove('navigation__logo-link--no-js');
-navWrapper.classList.remove('navigation--no-js');
+nav.classList.remove('navigation--no-js');
+navWrapper.classList.remove('navigation__wrapper--no-js');
 
 const onNavToggleClick = () => {
   let svgLink = 'opened';
@@ -23,6 +27,8 @@ const onNavToggleClick = () => {
   navLogo.classList.toggle('navigation__logo-link--opened');
   navMain.classList.toggle('navigation__list--closed');
   navMain.classList.toggle('navigation__list--opened');
+  pageBody.classList.toggle('page__body--nav-opened');
+  navHeadline.classList.toggle('navigation__headline--opened');
 
   if (navToggle.classList.contains('navigation__button--opened')) {
     navToggle.querySelector('span').textContent = 'Закрыть меню навигации';
@@ -31,7 +37,14 @@ const onNavToggleClick = () => {
     navToggle.querySelector('span').textContent = 'Открыть меню навигации';
   }
   navToggle.querySelector('use').setAttribute('xlink:href', `img/sprite.svg#navigation-icon-${svgLink}`);
+
 };
+
+navWrapper.addEventListener('mouseup', (evt) => {
+  if (!navMain.contains(evt.target)) {
+    onNavToggleClick();
+  }
+});
 
 navToggle.addEventListener('click', () => {
   onNavToggleClick();
